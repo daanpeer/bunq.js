3459c319dc7d15718cbc815048103388c681ceae
Daan Houweling <daan.gdh@gmail.com>
2017-04-15 23:14:30 +0200

    Added extra tests

--- test/bunq.test.js
+++ test/bunq.test.js
@@ -1,6 +1,6 @@
 const Bunq = require('../lib/bunq.js')
 const sign = require('../lib/helpers/sign')
-const Payments = require('../lib/api/Payments');
+const Payments = require('../lib/api/Payments')
 
 const {monetaryResponse, paymentsResponse} = require('./responses')
 
@@ -20,28 +20,61 @@ const config = {
 
 const API_URL = 'https://sandbox.public.api.bunq.com'
 
+const bunqInstance = () => {
+  const stub = sinon.stub({sign}, sign)
+  const bunq = new Bunq(config)
+  bunq.sign = stub
+  bunq.user = {id: 123}
+  return bunq
+}
+
 describe('Bunq api', () => {
 
-  beforeEach(() => {
-    const stub = sinon.stub({sign}, sign)
-    const bunq = new Bunq(config)
-    bunq.sign = stub
-    bunq.user = {id: 123}
-    this.bunq = bunq
+  it('Should throw an error when no options are specified', () => {
+    assert.throws(() => new Bunq(), 'Please specify the required options')
+  })
+
+  it('Should throw an error when no keypair is specified', () => {
+    assert.throws(() => new Bunq({apiKey: '123'}), 'Please specify an private and public key pair')
+  })
+
+  it('Should throw an error when no api key is specified', () => {
+    assert.throws(() => new Bunq({}), 'Please specify an api key in order to use the Bunq API')
+  })
+
+  it('Should switch to sandbox by default', () => {
+    const bunq = new Bunq({apiKey: '123', keyPair: {public: '123', private: '123'}})
+    assert.equal(bunq.apiUrl, API_URL)
+  })
+
+  it('Should should sign headers correctly', async () => {
+    // assert that headers are sorted
+    // assert that the sign method is called with the sorted headers
+    // assert that the new header is added
+    // assert that only x-bunq, cache-control and user-agent headers are added
+
+    // const headers = {
+    // };
+    //
+    // this.bunq.signHeaders()
   })
 
   it('Should retrieve monetaryAccounts', async () => {
+    const bunq = bunqInstance()
+
     nock(API_URL)
       .get('/v1/user/123/monetary-account')
       .reply(200, monetaryResponse)
 
-    const data = await this.bunq.monetaryAccounts().list()
+    const data = await bunq.monetaryAccounts().list()
 
     assert.isArray(data)
     assert.typeOf(data[0], 'Object')
   })
 
   it('Should retrieve payments based on a monetary account', async () => {
+    const bunq = bunqInstance()
+
     nock(API_URL)
       .get('/v1/user/123/monetary-account')
       .reply(200, monetaryResponse)
@@ -50,11 +83,11 @@ describe('Bunq api', () => {
       .get('/v1/user/123/monetary-account/1871/payment')
       .reply(200, paymentsResponse)
 
-    const data = await this.bunq.monetaryAccounts().list()
+    const data = await bunq.monetaryAccounts().list()
     const payments = await data[0].payments()
 
     assert.isArray(payments)
-    assert.deepEqual(paymentsResponse.Response, payments);
+    assert.deepEqual(paymentsResponse.Response, payments)
   })
 
 })
